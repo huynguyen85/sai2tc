9d76010a0fb54fd40808e588ac76ae25d60df513
Marian Pritsak <marianp@mellanox.com>
2019-06-03 15:36:40 +0300

    Build libsai.so with debug flag

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 rm libsai.so
-gcc -shared -o libsai.so -fPIC -I inc/sai/inc -I inc -Wall sai.c db.c sai_utilities.c
+gcc -g -shared -o libsai.so -fPIC -I inc/sai/inc -I inc -Wall sai.c db.c sai_utilities.c
 sudo install libsai.so /usr/local/lib
 ldconfig
 
